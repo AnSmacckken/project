44470de5526d04fb1950f6be2c55056928d0c983
AnSmacckken <anas56mkeen@gmail.com>
2026-01-05 22:39:36 +0200

    maken el 3adelo 3

--- src/utils/baseUrl.jsx
+++ src/utils/baseUrl.jsx
@@ -1,4 +1,4 @@
 // Use environment variable if available, otherwise use default
 export const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "https://ecommerce.routemisr.com/api/v1";
 
-export const baseUrlFrontend = process.env.NEXT_PUBLIC_FRONTEND_URL || "https://fresh-cart-ecommerce-three.vercel.app";
+export const baseUrlFrontend = process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
